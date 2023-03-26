8cabdb3d428a68a2bf3f90cea15b788f054c0657
Albert Thuku <athuku02@gmail.com>
2023-03-27 02:44:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const input = document.getElementById("input");
+
+const action = () => alert("Enter data here!")
+
 function addingEventListener() {
+    input.addEventListener("mouseover", action);
 }
